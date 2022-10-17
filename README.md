614fe89683bc2afc7fb0af2465bfc1c1f1ca488c
Angela <aangelahuang05@gmail.com>
2022-10-16 21:50:36 -0400

    1.4.1b removed minionese & added minion assets

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # MINION WEB EXTENSION
 
 **Functionality**
+
 -> Replaces img's with minions & converts text to Minionese - lingojamgibberish dialect (excluding google pages)
 
 -> Allows you to toggle better colours.
--- imgreplace.js
+++ imgreplace.js
@@ -1,6 +1,6 @@
 var images = document.getElementsByTagName('img')
 
-let replacements = ["./assets/minion1.png","./assets/minion2.jpg","./assets/minion3.jpg","./assets/minion4.jpg","./assets/minion5.jpg","./assets/minion6.jpg","./assets/minion7.jpg","./assets/minion8.png","./assets/minion9.jpeg","./assets/minion10.jpg","./assets/minion11.jpg","./assets/minion12.jpg","./assets/minion13.jpg"]
+let replacements = ["./assets/minion1.png","./assets/minion2.jpg","./assets/minion3.jpg","./assets/minion4.jpg","./assets/minion5.jpg","./assets/minion6.jpg","./assets/minion7.jpg","./assets/minion8.png","./assets/minion9.jpeg","./assets/minion10.jpg","./assets/minion11.jpg","./assets/minion12.jpg","./assets/minion13.jpg","./assets/minion14.png","./assets/minion15.jpg","./assets/minion16.png","./assets/minion17.jpg"]
 
 for (elt of images) {
     var imgReplacement = replacements[Math.floor(Math.random() * replacements.length)]
@@ -8,3 +8,9 @@ for (elt of images) {
     elt.srcset = `${browser.runtime.getURL(imgReplacement)}`
     elt.alt = 'minion'
 }
+
+// var videos = document.getElementsByTagName('source')
+
+// for (elt of videos) {
+//     elt.src= `${browser.runtime.getURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}`
+// }
